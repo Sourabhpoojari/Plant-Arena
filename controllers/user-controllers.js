c6aa94d0d105cef7ea7ea56b4f8f540e6c82cdbb
swaroop.r <swaroop.r@pacewisdom.com>
2020-11-19 15:41:29 +0530

    design extenstion

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -3,19 +3,19 @@ const User = require('../models/user'),
     bcrypt = require('bcryptjs'),
     jwt = require('jsonwebtoken'),
     config = require('config');
-
+const Swal = require('sweetalert2')
 // @route POST /user
 // @desc register user
 // @access Public
-const createUser = async (req,res,next) => {
-    const { name, email, password, phone} = req.body;
+const createUser = async (req, res, next) => {
+    const { name, email, password, phone } = req.body;
     let user;
     try {
-        user = await User.findOne({email});
+        user = await User.findOne({ email });
         if (user) {
-            return res.status(400).json({errors:[{msg : "User with this email'id already exists"}]});
+            return res.status(400).json({ errors: [{ msg: "User with this email'id already exists" }] });
         }
-        const avatar = gravatar.url(email,{
+        const avatar = gravatar.url(email, {
             s: '200',
             r: 'pg',
             d: 'mm'
@@ -29,22 +29,24 @@ const createUser = async (req,res,next) => {
         });
         // bcrypt password
         const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password,salt);
+        user.password = await bcrypt.hash(password, salt);
         await user.save();
 
         // token
         const payload = {
-            user : {
-                id : user.id
+            user: {
+                id: user.id
             }
         };
         jwt.sign(
             payload,
             config.get('jwtSecret'),
-            {expiresIn : 3600},
-            (err,token)=>{
+            { expiresIn: 3600 },
+            (err, token) => {
                 if (err) throw err;
-                res.status(200).json({token});
+                res.flash("success", "Successfully Signed up");
+                res.redirect('/Landing');
+                res.status(200).json({ token });
             }
         );
 
@@ -57,10 +59,11 @@ const createUser = async (req,res,next) => {
 // @route GET /user
 // @desc get user details
 // @access Private
-const getUser = async (req,res,next)=>{
+const getUser = async (req, res, next) => {
     try {
         const user = await User.findById(req.user.id).select('-password');
-        res.status(201).json({user});
+
+        res.status(201).json({ user });
         // give 
     } catch (err) {
         console.log(err);
@@ -71,39 +74,39 @@ const getUser = async (req,res,next)=>{
 // @route POST /login
 // @desc login user
 // @access Public
-const logIn = async (req,res,next) =>{
-    const {email, password} = req.body;
+const logIn = async (req, res, next) => {
+    const { email, password } = req.body;
     let user;
     try {
-        user = await User.findOne({email});
+        user = await User.findOne({ email });
         if (!user) {
-            return res.status(400).json({errors:[{msg : "invalid credentials"}]});
+            return res.status(400).json({ errors: [{ msg: "invalid credentials" }] });
         }
         // check password
         const match = await bcrypt.compare(password, user.password);
         if (!match) {
-            return res.status(400).json({errors:[{msg : "invalid credentials"}]});
+            return res.status(400).json({ errors: [{ msg: "invalid credentials" }] });
         }
         // setting jwt
         const payload = {
-            user : {
-                id : user.id
+            user: {
+                id: user.id
             }
         }
         jwt.sign(
             payload,
             config.get('jwtSecret'),
-            {expiresIn : 3600},
-            (err,token) =>{
-                if(err) throw err;
+            { expiresIn: 3600 },
+            (err, token) => {
+                if (err) throw err;
                 // use this token to login
-                // Swal.fire({
-                //     icon: 'success',
-                //     text: 'Logged in successfully!',
-                   
-                //   });
-                //res.redirect('/Landing');
-                 res.status(200).json({token});
+
+                console.log("login success");
+                console.log("user");
+                req.flash("success", "successfully Logged in");
+                res.redirect('/Landing');
+
+                res.status(200).json({ token });
             }
         );
     } catch (err) {
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken'),
     config = require('config');
 
-module.exports = (req,res,next)=>{
+module.exports = (req, res, next) => {
     const token = req.header('x-auth-token');
     // if no token
-    if(!token){
-        return res.status(401).json({msg:'No token found, authorization denied'});
+    if (!token) {
+        return res.status(401).json({ msg: 'No token found, authorization denied' });
     }
     // verify token 
     try {
-        const decoded = jwt.verify(token,config.get('jwtSecret'));
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
         req.user = decoded.user;
+
         next();
     } catch (err) {
-        res.status(401).json({msg:'Token is not valid'});
+        res.status(401).json({ msg: 'Token is not valid' });
     }
 };
